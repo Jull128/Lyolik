a43f00388dd2974fab4c7a2ecec965d97fce81e6
Jull128 <88882567+Jull128@users.noreply.github.com>
2023-04-16 23:43:33 +0500

    swiper beauty

--- src/components/PowerSpotlight/PowerSpotlight.jsx
+++ src/components/PowerSpotlight/PowerSpotlight.jsx
@@ -1,4 +1,4 @@
-
+import vector from '../GeToday/Swiper/Vector.svg'
 import style from './style.module.css'
 import people from './people.png'
 import india from './GEIndia.png'
@@ -20,67 +20,76 @@ export function PowerSpotlight() {
 
     return (
         <div className={style.container}>
-            <>
-                <Swiper
-                    slidesPerView={4}
-                    spaceBetween={40}
-                    speed={2500}
-                    freeMode={true}
-                    autoplay={{
-                        delay: 3500,
-                        disableOnInteraction: false,
-                    }}
-                    modules={[Autoplay, FreeMode]}
-                    className={style.swiper}
+            <div className={style.box}>
+                <div className={style.description__container}>
+                    <div className={style.button__box}>
+                        <button className={style.button_big}>Visit the newsroom</button>
+                        <button className={style.button_small}><img src={vector} alt="" className={style.vector} /> </button>
+                    </div>
+                    <div className={style.description__text}>Power Spotlight</div>
+                </div>
+                <>
+                    <Swiper
+                        slidesPerView={4}
+                        spaceBetween={40}
+                        speed={2500}
+                        freeMode={true}
+                        autoplay={{
+                            delay: 3500,
+                            disableOnInteraction: false,
+                        }}
+                        modules={[Autoplay, FreeMode]}
+                        className={style.swiper}
 
-                >
-                    <SwiperSlide className={style.slide_1}>001
-                        <div className={style.slide_box}>
-                            <p className={style.title}>People</p>
-                            <img src={people} alt='' className={style.picture} />
-                            <div className={style.description}>
-                                <p>Electrification and Decarbonization: The UAE as a Springboard for Action for 2023’s Top Two Priorities</p>
+                    >
+                        <SwiperSlide className={style.slide_1}>001
+                            <div className={style.slide_box}>
+                                <p className={style.title}>People</p>
+                                <img src={people} alt='' className={style.picture} />
+                                <div className={style.description}>
+                                    <p>Electrification and Decarbonization: The UAE as a Springboard for Action for 2023’s Top Two Priorities</p>
+                                </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className={style.slide_2}>002
-                        <div className={style.slide_box}>
-                            <p className={style.title}>Sustainability</p>
-                            <img src={sustainability} alt='' className={style.picture} />
-                            <div className={style.description}>
-                                <p>Like Father, Like Son: How Chris Day Followed His Dad Into GE’s Engineering World</p>
+                        </SwiperSlide>
+                        <SwiperSlide className={style.slide_2}>002
+                            <div className={style.slide_box}>
+                                <p className={style.title}>Sustainability</p>
+                                <img src={sustainability} alt='' className={style.picture} />
+                                <div className={style.description}>
+                                    <p>Like Father, Like Son: How Chris Day Followed His Dad Into GE’s Engineering World</p>
+                                </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className={style.slide_1}>003
-                        <div className={style.slide_box}>
-                            <p className={style.title}>GE India</p>
-                            <img src={india} alt='' className={style.picture} />
-                            <div className={style.description}>
-                                <p>Faces of the Future: Meet 3 People Making a Difference at GE India as It Celebrates 120 Years of Innovation</p>
+                        </SwiperSlide>
+                        <SwiperSlide className={style.slide_1}>003
+                            <div className={style.slide_box}>
+                                <p className={style.title}>GE India</p>
+                                <img src={india} alt='' className={style.picture} />
+                                <div className={style.description}>
+                                    <p>Faces of the Future: Meet 3 People Making a Difference at GE India as It Celebrates 120 Years of Innovation</p>
+                                </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className={style.slide_2}>004
-                        <div className={style.slide_box}>
-                            <p className={style.title}>Empowering women</p>
-                            <img src={women} alt='' className={style.picture} />
-                            <div className={style.description}>
-                                <p>Winds of Change: Women Are Now Calling the Shots at This Giant Turbine Blade Factory in India</p>
+                        </SwiperSlide>
+                        <SwiperSlide className={style.slide_2}>004
+                            <div className={style.slide_box}>
+                                <p className={style.title}>Empowering women</p>
+                                <img src={women} alt='' className={style.picture} />
+                                <div className={style.description}>
+                                    <p>Winds of Change: Women Are Now Calling the Shots at This Giant Turbine Blade Factory in India</p>
+                                </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className={style.slide_1}>005
-                        <div className={style.slide_box}>
-                            <p className={style.title}>Future of energy</p>
-                            <img src={future} alt='' className={style.picture} />
-                            <div className={style.description}>
-                                <p>Green Light: Demonstration Project on Long Island Shows a Promising Path Forward for Green Hydrogen</p>
+                        </SwiperSlide>
+                        <SwiperSlide className={style.slide_1}>005
+                            <div className={style.slide_box}>
+                                <p className={style.title}>Future of energy</p>
+                                <img src={future} alt='' className={style.picture} />
+                                <div className={style.description}>
+                                    <p>Green Light: Demonstration Project on Long Island Shows a Promising Path Forward for Green Hydrogen</p>
+                                </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                </Swiper>
-            </>
+                        </SwiperSlide>
+                    </Swiper>
+                </>
+            </div>
         </div >
     )
 }
