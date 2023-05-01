baa4b0e487441ea1dcdcbe4fd460bac355a5ac7f
Jull128 <88882567+Jull128@users.noreply.github.com>
2023-05-02 01:20:23 +0500

    modal news

--- src/components/GeToday/GeToday.jsx
+++ src/components/GeToday/GeToday.jsx
@@ -1,16 +1,22 @@
 import { Swiper } from './Swiper/Swiper'
 import style from './style.module.css'
 import vector from './Swiper/Vector.svg'
+import { useNavigate } from 'react-router-dom'
+
 
 export function GeToday() {
+    const navigate = useNavigate()
 
+    const openNews = () => {
+        navigate('/news')
+    }
 
     return (
         <div className={style.container}>
             <div className={style.box}>
                 <div className={style.button__box}>
-                    <button className={style.button_big}>Visit the newsroom</button>
-                    <button className={style.button_small}><img src={vector} alt="" className={style.vector} /> </button>
+                    <button onClick={openNews} className={style.button_big}>Visit the newsroom</button>
+                    <button onClick={openNews} className={style.button_small}><img src={vector} alt="" className={style.vector} /> </button>
                 </div>
 
                 <div className={style.title__box}>
--- src/page/News/News.jsx
+++ src/page/News/News.jsx
@@ -1,5 +1,23 @@
+import { useQuery } from '@tanstack/react-query'
+import style from './style.module.css'
+
 export function News() {
+    const { data, isLoading, isError, error } = useQuery({
+        queryKey: ['news'],
+        queryFn: () => fetch('http://localhost:3005/news').then(res => res.json())
+    })
     return (
-        <></>
+        <div className={style.container}>
+            <div className={style.news__box}>
+                {data?.map((feed) => (
+                    <div key={feed['id']} className={style.card}>
+                        <img src={feed.img} alt="" className={style.img} />
+                        <p className={style.title__card}>
+                            {` ${feed.title}`}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
